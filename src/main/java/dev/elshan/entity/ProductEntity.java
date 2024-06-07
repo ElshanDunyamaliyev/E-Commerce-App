cd81495f4e3d5aab2c419bccf7bac7f8bc9f4688
ElshanDunyamaliyev <elshandunyamaliyev@gmail.com>
2024-06-07 11:19:32 +0400

    added productEntity and productDto

---
+++ src/main/java/dev/elshan/entity/ProductEntity.java
@@ -0,0 +1,29 @@
+package dev.elshan.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long productId;
+
+    @NotBlank
+    @Size(min = 3, message = "Product name must contain at least 3 characters")
+    @Column(name = "product_name")
+    private String productName;
+
+    private String image;
+
+    @NotBlank
+    @Size(min = 6, message = "Product description must contain at least 6 characters")
+    private String description;
+
+    private Integer quantity;
+    private double price;
+    private double discount;
+}
